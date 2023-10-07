7a7efb0ec7fe23b511de01457fefeb515d335be9
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-07 13:50:11 -0400

    implement insert()

--- files/useLinkedList.js
+++ files/useLinkedList.js
@@ -2,7 +2,7 @@ const LinkedList = require("./linkedList");
 
 const linkedList = new LinkedList();
 
-linkedList.insertAtHead("One");
-linkedList.insertAtHead(2);
+linkedList.insert("One");
+linkedList.insert(2);
 
 console.log(linkedList);
