a7d7f7e6038481ab47442311c847332f4cb1adae
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-07 13:55:30 -0400

    update insert() funct to use the find() func

--- files/useLinkedList.js
+++ files/useLinkedList.js
@@ -1,23 +1,8 @@
-const LinkedList = require("./linkedList");
-
 const linkedList = new LinkedList();
 
 linkedList.insert("One");
 linkedList.insert(2);
 
-// You can use `find()` to update the value of a node in the list.
-linkedList.find((node) => node.value === "One").value = 1;
+linkedList.insert(1.5, (node) => node.value === "One");
 
 console.log(linkedList);
-
-console.log(
-  "find 2",
-  linkedList.find((node) => node.value === 2)
-);
-
-console.log(
-  "find 3",
-  linkedList.find((node) => node.value === 3)
-);
-//to find a node by index only:
-console.log(linkedList.find((node, index) => index === 1));
